767f98980c16a9c2298079adff378880befcb560
Alexander Smoktal <cosm.ua@gmail.com>
2024-05-06 18:53:54 +0300

    Fix README code

--- karo-common-rpc/src/lib.rs
+++ karo-common-rpc/src/lib.rs
@@ -13,62 +13,85 @@ Use [rpc::Rpc::poll] method to poll the stream. This includes waiting for a call
 
 RPC calls:
 ```
-let stream = UnixStream::connect("/tmp/hub.sock").await.unwrap();
-let mut rpc = Rpc::new(stream);
+use futures::{select, FutureExt};
+use tokio::net::UnixStream;
 
-let call = rpc.call::<u32, u32>("echo", &42).await.unwrap();
+use karo_common_rpc::rpc::Rpc;
 
-select! {
-    response = call.fuse() => {
-        println!("Call response: {response}")
-    },
-    _ = rpc.poll().fuse() => {}
+async fn call() {
+    let stream = UnixStream::connect("/tmp/hub.sock").await.unwrap();
+    let mut rpc = Rpc::new(stream);
+
+    let call = rpc.call::<u32, u32>("echo", &42).await.unwrap();
+
+    select! {
+        response = call.fuse() => {
+            println!("Call response: {response:?}")
+        },
+        _ = rpc.poll().fuse() => {}
+    }
 }
 ```
 
 RPC subscription:
 ```
-let stream = UnixStream::connect("/tmp/hub.sock").await.unwrap();
-let mut rpc = Rpc::new(stream);
+use futures::{select, FutureExt, StreamExt};
+use tokio::net::UnixStream;
 
-let subscription = subscribe::<u32>("subscription", &42).await.unwrap();
+use karo_common_rpc::rpc::Rpc;
 
-select! {
-    response = subscription.take(2).collect::<Vec<Result<u32>>>() => {
-        println!("Subscription response: {response}")
-    },
-    _ = rpc.poll().fuse() => {}
+async fn subscribe() {
+    let stream = UnixStream::connect("/tmp/hub.sock").await.unwrap();
+    let mut rpc = Rpc::new(stream);
+
+    let subscription = rpc.subscribe::<u32>("signal").await.unwrap();
+
+    select! {
+        response = subscription.take(2).collect::<Vec<karo_common_rpc::Result<u32>>>() => {
+            println!("Subscription response: {response:?}")
+        },
+        _ = rpc.poll().fuse() => {}
+    }
 }
 ```
 
 Polling imcoming messages:
 ```
-let stream = UnixStream::connect("/tmp/hub.sock").await.unwrap();
-let mut rpc = Rpc::new(stream);
+use futures::{select, FutureExt};
+use tokio::net::UnixStream;
 
-while true {
-    let request = rpc.poll().await {
+use karo_common_rpc::{rpc::Rpc, request::Body};
+
+async fn poll() {
+    let stream = UnixStream::connect("/tmp/hub.sock").await.unwrap();
+    let mut rpc = Rpc::new(stream);
+
+    loop {
+        let request = rpc.poll().await;
+
         if request.is_none() {
             println!("Client disconnected");
             return;
         }
-    }
 
-    println!("Incoming method call: {}", request.endpoint());
-    match request.take_body().unwrap() {
-        Body::Call(bson) => {
-            println!("Incoming call: {bson:?}");
-            request.respond(Ok(bson))
-        },
-        Body::Subscription => {
-            println!("Incoming subscription");
-            request.respond(Ok(41));
-            request.respond(Ok(42));
-            request.respond(Ok(43));
-        },
-        Body::Fd(client_name, _) => {
-            println!("Incoming connection request from {client_name}");
-            request.respond(Ok(()))
+        let mut request = request.unwrap();
+
+        println!("Incoming method call: {}", request.endpoint());
+        match request.take_body().unwrap() {
+            Body::Call(bson) => {
+                println!("Incoming call: {bson:?}");
+                request.respond(Ok(bson)).await;
+            },
+            Body::Subscription => {
+                println!("Incoming subscription");
+                request.respond(Ok(41)).await;
+                request.respond(Ok(42)).await;
+                request.respond(Ok(43)).await;
+            },
+            Body::Fd(client_name, _) => {
+                println!("Incoming connection request from {client_name}");
+                request.respond(Ok(())).await;
+            }
         }
     }
 }
