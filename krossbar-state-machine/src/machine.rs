1781261c0b529d4256da2fadec760a26b805ba90
Alexander Smoktal <cosm.ua@gmail.com>
2024-06-12 21:44:57 +0300

    Remove debug print

--- krossbar-state-machine/src/machine.rs
+++ krossbar-state-machine/src/machine.rs
@@ -49,7 +49,6 @@ where
     type Output = Result<St, Err>;
 
     fn poll(mut self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Self::Output> {
-        println!("YOBA - 3");
         Poll::Ready(Ok(self.state.take().unwrap()))
     }
 }
